6318cc0f7351a5ad355dd73326e09b42e20ff3e2
jose luis <jose.l.condori@gmail.com>
2022-10-28 21:16:22 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let menuMobile=document.querySelector('#menuMobile')
+let menu=document.querySelector('#menu')
+
+menuMobile.addEventListener('click',()=>{
+    menu.classList.toggle('miclase')
+    
+})
